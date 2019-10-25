9856eab729d0977df0f41906e255add6240b5bb4
DaveTBlake <oak99sky@yahoo.co.uk>
2019-10-25 15:05:56 +0100

    GetArtistsByWhere and GetArtistsByWhereJSON use SQL limit sorting Use USEARTISTSORTNAME setting for songs, albums and artists Fix field renaming in GetSongsByWhereJSON GetRecentlyPlayedAlbums - add timing for tests

--- xbmc/filesystem/SmartPlaylistDirectory.cpp
+++ xbmc/filesystem/SmartPlaylistDirectory.cpp
@@ -61,6 +61,8 @@ namespace XFILE
     sorting.sortAttributes = playlist.GetOrderAttributes();
     if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_FILELISTS_IGNORETHEWHENSORTING))
       sorting.sortAttributes = (SortAttribute)(sorting.sortAttributes | SortAttributeIgnoreArticle);
+    if (playlist.IsMusicType() && CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_MUSICLIBRARY_USEARTISTSORTNAME))
+      sorting.sortAttributes = static_cast<SortAttribute>(sorting.sortAttributes | SortAttributeUseArtistSortName);
     items.SetSortIgnoreFolders((sorting.sortAttributes & SortAttributeIgnoreFolders) == SortAttributeIgnoreFolders);
 
     std::string option = !filter ? "xsp" : "filter";
